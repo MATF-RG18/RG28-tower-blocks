179075550a41182b2da5f7b966fb262435a9e845
StefanJ996 <programiranje25@gmail.com>
2019-05-22 13:20:11 +0200

    Ubacen score i modifikovan .gitignore

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <GL/glut.h>
 #include <math.h>
-
+#include <string.h>
 
 #define TIMER_ID 0
 #define TIMER_INTERVAL 20
@@ -42,18 +42,20 @@ struct Koordinate{
 // Niz tipa Koordinate u koji smestamo sve kutije koje napravimo
 struct Koordinate* niz;
 
-static int animation_ongoing;
+int animation_ongoing;
 void on_display(void);
 void draw(GLfloat x,GLfloat y,GLfloat z,GLfloat nx,GLfloat ny,GLfloat nz);
-static void on_timer(int value);
+void on_timer(int value);
 void graphicsInit();
+void drawScore() ;
 void keyboard(unsigned char key, int x, int y);
 
 int main(int argc, char ** argv)
 {
-    // Kreiranje prostora za niz
-    niz = malloc(100 * sizeof(struct Koordinate));
+    // Kreiranje prostora za niz koordinata kutija
+    niz = malloc(50 * sizeof(struct Koordinate));
     if(niz == NULL){
+        fprintf(stderr, " Greska u alokaciji memorije");
         exit(1);
     }
 
@@ -61,12 +63,17 @@ int main(int argc, char ** argv)
 
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+
+    // Funkcija za kreiranje prozora i iscrtavanje
     graphicsInit();
+
+    // Funkcija koja se poziva nakon pritiska na taster tastature
     glutKeyboardFunc(keyboard);
 
     // Program ulazi u glavnu petlju
     glutMainLoop();
 
+    free(niz);
     return 0;
 }
 
@@ -77,19 +84,28 @@ void on_display(void)
     // Brise se prethodni sadrzaj prozora
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    // Postavljamo crvenu boju za ispis skora igraca
+    glColor3f(1,0,0);
+
+    // Ispisujemo trenutni skor
+    drawScore();
+
     // Crta se kutija koja se krece
     draw(x,y,z,nx,ny,nz);
 
     // U ovoj petlji se crtaju vec polozene kutije
     for(int j = 0; j <= i;j++){
         if(j>1) {
-                    // Ovo je provera da li je kutija spustena na kutiju ili pored nje, ako je pored izlazi se iz igre
+            // Ovo je provera da li je kutija spustena na kutiju ili pored nje, ako je pored izlazi se iz igre
             if (fabsf((niz[j - 2].a - niz[j-1].a)) > 0.2 && fabsf((niz[j - 2].d - niz[j-1].d)) > 0.2) {
                 exit(1);
-            } else {
+            }
+            else {
                 draw(niz[j].a, niz[j].b, niz[j].c, niz[j].d, niz[j].e, niz[j].f);
             }
         }
+
+        //  Za prvu kutiju ne treba proveravati udaljenost, zbog toga idemo u else granu
         else {
                 draw(niz[j].a, niz[j].b, niz[j].c, niz[j].d, niz[j].e, niz[j].f);
         }
@@ -107,10 +123,10 @@ void graphicsInit() {
     glutInitWindowPosition(100, 100);
     glutCreateWindow("Build a Tower");
 
-    // Pozivanje callback funkcije i postavljanje boje pozadine
+    // Pozivanje callback funkcije
     glutDisplayFunc(on_display);
 
-    // Pstavlja se boja pozadine
+    // Postavlja se boja pozadine
     glClearColor(0.3, 0.5, 0.5, 0.3);
 }
 
@@ -120,42 +136,42 @@ void draw(GLfloat x,GLfloat y,GLfloat z,GLfloat nx,GLfloat ny,GLfloat nz){
     glBegin(GL_QUADS);
 
     // Gornji deo
-    glColor3f(0.0f, 1.0f, 0.0f);
+    glColor3f(1, 1, 0);
     glVertex3f(x, y, nz);
     glVertex3f(nx, y, nz);
     glVertex3f(nx, y, z);
     glVertex3f(x, y, z);
 
     // Donji deo
-    glColor3f(1.0f, 0.5f, 0.0f);
+    glColor3f(1, 1, 0);
     glVertex3f(x, ny, z);
     glVertex3f(nx, ny, z);
     glVertex3f(nx, ny, nz);
     glVertex3f(x, ny, nz);
 
     // Napred
-    glColor3f(1.0f, 0.0f, 0.0f);
+    glColor3f(1, 1, 0);
     glVertex3f(x, y, z);
     glVertex3f(nx, y, z);
     glVertex3f(nx, ny, z);
     glVertex3f(x, ny, z);
 
     // Nazad
-    glColor3f(1.0f, 1.0f, 0.0f);
+    glColor3f(1, 1, 0);
     glVertex3f(x, ny, nz);
     glVertex3f(nx, ny, nz);
     glVertex3f(nx, y, nz);
     glVertex3f(x, y, nz);
 
     // Levo
-    glColor3f(0.0f, 0.0f, 1.0f);
+    glColor3f(1, 1, 0);
     glVertex3f(nx, y, z);
     glVertex3f(nx, y, nz);
     glVertex3f(nx, ny, nz);
     glVertex3f(nx, ny, z);
 
     // Desno
-    glColor3f(0.3f, 0.4f, 0.2f);
+    glColor3f(1, 1, 0);
     glVertex3f(x, y, nz);
     glVertex3f(x, y, z);
     glVertex3f(x, ny, z);
@@ -169,7 +185,7 @@ void keyboard(unsigned char key, int x, int y)
     switch (key)
         {
 
-            // Pustanje kutije na dole
+            // Pokretanje kutije nadole
         case 'd':
         case 'D':
             p = 1;
@@ -187,7 +203,7 @@ void keyboard(unsigned char key, int x, int y)
 
         case 's':
         case 'S':
-            // Pokrece se animacija.
+            // Pokrece se igra
             if (!animation_ongoing) {
                 glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
                 animation_ongoing = 1;
@@ -195,18 +211,37 @@ void keyboard(unsigned char key, int x, int y)
             break;
 
     }
-    glutPostRedisplay();
-}
 
-static void on_timer(int value)
+}
+void drawScore() {
+
+    // Deklaracija stringa u koji smestamo skor igraca
+    char c[13];
+
+    // Smestanje skora u promenljivu c, za svaku kutiju se dobija 100 poena
+    snprintf(c,13,"SCORE : %d  ",i*100);
+
+    // Funkcija kojoj prosledjujemo koordinate gde zelimo da se nalazi nas ispis
+    glRasterPos3f(0.4, 0.6, 0.2);
+
+    // U petlji prolazimo kroz string i ispisujemo trenutni skor igraca uz pomoc funkcije glutBitmapCharacter()
+    for (int b=0; b < 13; b++)
+    {
+
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, c[b]);
+    }
+
+
+}
+void on_timer(int value)
 {
 
-     // Proverava se da li callback dolazi od odgovarajuceg tajmera.
+     // Proverava se da li callback dolazi od odgovarajuceg tajmera
 
     if (value != TIMER_ID)
         return;
 
-    // Kretanje kutije desno.
+    // Kretanje kutije desno
     if(x<=0.5 && t == 1 && p==0) {
         if(x>=0.499 && x<= 0.501)
             t=0;
@@ -234,16 +269,18 @@ static void on_timer(int value)
         niz[i].d = nx;
         niz[i].e = ny;
         niz[i].f = nz;
-        if (i < 50 && ny < -0.98 + k && y < -0.98 + k){
+
+    // Za svaku kutiju povecavamo k i dodajemo ga na y koordinatu, da bi kutije "nalegle" jedna na drugu prilikom spustanja
+        if (ny < -0.98 + k && y < -0.98 + k){
             i++;
             k = k + 0.02;
 
         }
 
-        // Postavljanje p na 0 da kutija ne bi isla na dole dok ne pritisnemo 'd'
+        // Postavljanje p na 0 da kutija ne bi isla nadole dok ne pritisnemo 'd'
         p=0;
 
-        // Vracanje pocetnih vrednosti za kutiju
+        // Vracanje pocetnih vrednosti koordinata za kutiju
         y =Y;
         ny = NY;
 
